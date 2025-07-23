fb353d3321983f60d1ebb255225fab5412caf7e1
jacekMe <jacekmeres@gmail.com>
2025-07-23 22:56:38 +0200

    use of mathematical functions

--- module2_built-in_functions.sql
+++ module2_built-in_functions.sql
@@ -34,10 +34,50 @@ SELECT ProductID, Name
 FROM SalesLT.Product
 WHERE ProductID % 2 = 0
  
---==== MULTIPLICATION ====--
+--==== EXPONENTIATION ====--
 -- Oblicz kwadrat ceny katalogowej każdego produktu
 SELECT Name, ListPrice,
     POWER(ListPrice, 2) ListPriceSquared
 FROM SalesLT.Product
 WHERE ListPrice > 0
+
+
+--======== MATHEMATICAL FUNCTIONS ========--
+
+--==== ABS ====--
+-- Zwraca wartość bezwzgledną danej liczby
+SELECT ABS(-10) absolute_value
+
+--==== ROUND ====--
+-- Zaokrągla daną liczbę do określonej liczby miejsc po przecinku
+SELECT ROUND(3.14159, 2) rounded_value
+
+--==== CEILING ====--
+-- Zaokrągla daną liczbę w górę do najbliższej liczby całkowitej
+SELECT CEILING(4.3) rounded_up_value
+
+--==== FLOOR ====--
+--  Zaokrągla daną liczbę w dół do najbliższej liczby całkowitej
+SELECT FLOOR(4.7) rounded_down_value
+
+--==== SQRT ====--
+-- Zwraca pierwiastek kwadratowy dane liczby
+SELECT SQRT(25) square_root
+
+--==== EXP ====--
+-- Podnosi liczbę 'e' do potęgi danej liczby
+SELECT EXP(2) exponentiation
+
+--==== LOG ====--
+-- Zwraca logarytm naturalny danej liczby
+SELECT LOG(10) natural_logarithm
+
+--==== POWER ====--
+-- Podnosi podaną liczbę do określonej potęgi
+SELECT POWER(2, 3) result
+
+--==== SIGN ====--
+-- Zwraca znak danej liczby
+SELECT SIGN(3240) sign_value
+
 
